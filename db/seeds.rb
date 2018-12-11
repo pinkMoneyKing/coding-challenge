67283faa196ea74ce359882ec9daa9e76c547493
pinkMoneyKing <pink_money_king@protonmail.com>
2018-12-11 16:15:04 -0500

    reseeded with comments for posts

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,9 +8,9 @@
 #
 #   Seed out DB so we have stuff to start with
 
-Post.create(title: "Post one", body: "I am body for post one")
-Post.create(title: "Post two", body: "I am body for post two")
+post_one = Post.create({title: "Post one", body: "I am body for post one"})
+post_two = Post.create({title: "Post two", body: "I am body for post two"})
 
-# post_one.comments.create(body: "I am a comment for post one")
-# post_two.comments.create(body: "I am a comment for post two")
+post_one.comments.create({body: "I am a comment for post one"})
+post_two.comments.create({body: "I am a comment for post two"})
 
